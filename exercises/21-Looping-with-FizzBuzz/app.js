2031560a859440c7fd0d0abf60db095baff46c50
Samuel <samuelvalerin@gmail.com>
2021-02-18 18:10:18 -0600

    exercise 21 modifided

--- exercises/21-Looping-with-FizzBuzz/app.js
+++ exercises/21-Looping-with-FizzBuzz/app.js
@@ -1,7 +1,7 @@
 function fizzBuzz() {  
 	// Your code here
 	for(var i = 1; i <= 100; i++){
-        if(i % 3 == 0 && i % 5 == 0 && i % 15 == 0){
+        if(i % 3 == 0 && i % 5 == 0){
             console.log('FizzBuzz');
         }else if (i % 5 == 0) {
             console.log('Buzz');
